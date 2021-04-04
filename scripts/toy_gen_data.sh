1f4eb25f42066b5d6c6c16af88479082926452cd
Guillaume-Barthe <79014176+Guillaume-Barthe@users.noreply.github.com>
2021-04-04 11:07:58 +0200

    Update toy_gen_data.sh

--- scripts/toy_gen_data.sh
+++ scripts/toy_gen_data.sh
@@ -1,10 +1,10 @@
 for ty in "train" "test" ; do
-    rm -rf data/$ty/sr5
-    mkdir -p data/$ty/sr5
+    rm -rf data/$ty/sr5_20
+    mkdir -p data/$ty/sr5_20
     for i in {1..2}; do
-	rm -rf dimacs/$ty/sr5/grp$i
-	mkdir -p dimacs/$ty/sr5/grp$i
-	python3 python/gen_sr_dimacs.py dimacs/$ty/sr5/grp$i 10 --min_n 5 --max_n 10
-	python3 python/dimacs_to_data.py dimacs/$ty/sr5/grp$i data/$ty/sr5 60000
+	rm -rf dimacs/$ty/sr5_20/grp$i
+	mkdir -p dimacs/$ty/sr5_20/grp$i
+	python3 python/gen_sr_dimacs.py dimacs/$ty/sr5_20/grp$i 10000 --min_n 5 --max_n 20     #Modified to run on 10.000 pairs of problems with n in (5,20)
+	python3 python/dimacs_to_data.py dimacs/$ty/sr5_20/grp$i data/$ty/sr5_20 12000 	     #Modified to have only 12.000 max nodes per batch
     done;
 done;
